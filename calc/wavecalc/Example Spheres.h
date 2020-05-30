f6832e49fdc80e110b8078b601121c7dd5156a84
ronnyjamesdisco <53508110+ronnyjamesdisco@users.noreply.github.com>
2020-05-30 14:00:37 +0200

    first brainstorm

---
+++ calc/wavecalc/Example Spheres.h
@@ -0,0 +1,14 @@
+// This file generated with wavecalc
+// Authors: Hugo Paris, hugoplho@gmail.com, Dan Green danngreen1@gmail.com 
+//
+// 1) Place this file into SWN_PROJECT_DIR/inc/spheres/
+// 2) Add the following line to the top of SWN_PROJECT_DIR/inc/spheres_internal.h:
+//	#include "spheres/Example Spheres.h"
+// 3) Add (void *)Example Spheres inside the declaration for const void *wavetable_list[]={...} in spheres_internal.h
+// -------------------------------------------------------
+//
+// Wavetable name: Example Spheres
+// Wavetable by: Dan Green
+// Wavefiles for waveforms source directory: /mnt/c/Program Files/SphereEdit/Example Spheres/
+// oscillator / lfo: OSC
+
